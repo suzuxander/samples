db0e47dce01a4c3dd32c75884c56baeca223e768
suzuxander <suzuxander@gmail.com>
2020-03-11 20:51:06 +0900

    Update common

--- sample000/export.py
+++ sample000/export.py
@@ -6,3 +6,5 @@ class ExportResourceEnum(Enum):
     BUCKET_ARN = 'sample-bucket-arn'
     LAMBDA_SERVICE_ROLE_ARN = 'sample-lambda-service-role-arn'
     CODE_BUILD_SERVICE_ROLE_ARN = 'sample-code-build-service-role-arn'
+    CODE_PIPELINE_SERVICE_ROLE_ARN = 'sample-code-pipeline-service-role-arn'
+    CLOUD_FORMATION_SERVICE_ROLE_ARN = 'sample-cloud-formation-service-role-arn'
--- sample000/role.py
+++ sample000/role.py
@@ -8,6 +8,8 @@ def create_service_role() -> Template:
     template = Template()
     __create_lambda_function_service_role(template)
     __create_codebuild_service_role(template)
+    __create_codepipeline_service_role(template)
+    __create_cloudformation_service_role(template)
     return template
 
 
@@ -71,6 +73,90 @@ def __create_codebuild_service_role(template):
                             {
                                 "Action": [
                                     'logs:*',
+                                    's3:*',
+                                ],
+                                "Resource": [
+                                    '*'
+                                ],
+                                "Effect": "Allow"
+                            }
+                        ]
+                    }
+                )
+            ]
+        )
+    )
+    add_export(template, role.title + 'Arn', GetAtt(role, 'Arn'))
+
+
+def __create_codepipeline_service_role(template):
+    role = template.add_resource(
+        resource=Role(
+            title='SampleCodePipelineServiceRole',
+            RoleName='sample-codepipeline-service-role',
+            Path='/',
+            AssumeRolePolicyDocument={
+                'Statement': [{
+                    'Effect': 'Allow',
+                    'Principal': {
+                        'Service': 'codepipeline.amazonaws.com'
+                    },
+                    'Action': ['sts:AssumeRole']
+                }]
+            },
+            Policies=[
+                Policy(
+                    PolicyName='sample-codepipeline-policy',
+                    PolicyDocument={
+                        'Version': '2012-10-17',
+                        'Statement': [
+                            {
+                                "Action": [
+                                    'cloudformation:*',
+                                    'codebuild:*',
+                                    'iam:*',
+                                    's3:*',
+                                ],
+                                "Resource": [
+                                    '*'
+                                ],
+                                "Effect": "Allow"
+                            }
+                        ]
+                    }
+                )
+            ]
+        )
+    )
+    add_export(template, role.title + 'Arn', GetAtt(role, 'Arn'))
+
+
+def __create_cloudformation_service_role(template):
+    role = template.add_resource(
+        resource=Role(
+            title='SampleCloudFormationServiceRole',
+            RoleName='sample-cloudformation-service-role',
+            Path='/',
+            AssumeRolePolicyDocument={
+                'Statement': [{
+                    'Effect': 'Allow',
+                    'Principal': {
+                        'Service': 'cloudformation.amazonaws.com'
+                    },
+                    'Action': ['sts:AssumeRole']
+                }]
+            },
+            Policies=[
+                Policy(
+                    PolicyName='sample-cloudformation-policy',
+                    PolicyDocument={
+                        'Version': '2012-10-17',
+                        'Statement': [
+                            {
+                                "Action": [
+                                    'cloudformation:*',
+                                    'iam:*',
+                                    'lambda:*',
                                     's3:*',
                                 ],
                                 "Resource": [
